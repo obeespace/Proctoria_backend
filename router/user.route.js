14d8c9088b5685b28bde600baa096536a5103b33
obeespace <94024823+obeespace@users.noreply.github.com>
2024-07-07 18:39:01 +0100

    changed require to import

--- router/user.route.js
+++ router/user.route.js
@@ -1,4 +1,6 @@
-const express = require('express')
+// const express = require('express')
+import express from "express";
+
 const router = express.Router()
 
 const {signup, signin} = require('../controller/user.contoller.js')
